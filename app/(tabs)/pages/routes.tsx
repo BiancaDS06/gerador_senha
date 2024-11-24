ea3a2a702129d6ce91bc16f839bd35b9326d6009
Bianca Dias <bianca.dias.05@hotmail.com>
2024-11-24 17:14:09 -0300

    adiciona icones no menu

--- app/(tabs)/pages/routes.tsx
+++ app/(tabs)/pages/routes.tsx
@@ -2,14 +2,40 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from './home'
 import { Passwords } from "./passwords";
 
+import { Ionicons } from "@expo/vector-icons";
+
 const Tab = createBottomTabNavigator();
 
 export function Routes(){
     return (
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Home" component={Home}
+        options={{
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ focused, size, color }) => {
+            if(focused) {
+              return <Ionicons size={size} color={color} name="home" />
+            }
 
-        <Tab.Screen name="Password" component={Passwords} />
+            return <Ionicons size={size} color={color} name="home-outline" />
+            }
+        }}
+        />
+
+        <Tab.Screen name="Password" component={Passwords} 
+        options={{
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarIcon: ({ focused, size, color }) => {
+            if(focused) {
+              return <Ionicons size={size} color={color} name="lock-closed" />
+            }
+
+            return <Ionicons size={size} color={color} name="lock-closed-outline" />
+            }
+        }}
+        />
       </Tab.Navigator>
-    );
+    )
 }
